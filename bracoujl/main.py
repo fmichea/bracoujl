8d50e86ee2c17b8b3eb6eb8dcca056ff6cdfc475
Franck Michea <franck.michea@gmail.com>
2013-09-04 13:40:55 +0200

    Move main file in the package.

--- bracoujl/main.py
+++ bracoujl/main.py
@@ -13,17 +13,12 @@ import bracoujl.writers.asciiwriter
 
 def main():
     parser = argparse.ArgumentParser(description='Some debugging tool.')
-    parser.add_argument('-o', '--output', action='store', required=False,
-                        metavar='directory', help='output directory.')
+    parser.add_argument('-o', '--output', action='store', required=True,
+                        metavar='dir', help='output directory')
     parser.add_argument('log', action='append', nargs='+',
-                        help='log file correctly formatted.')
+                        help='log file correctly formatted')
     args = parser.parse_args(sys.argv[1:])
 
-#    output_filename = args.output if args.output else none
-#    writer = bracoujl.writers.dotwriter.dotwriter(output_filename)
-#    writer.generate(args.log[0])
-#
-#    sys.exit(0)
 
 if __name__ == '__main__':
     main()
