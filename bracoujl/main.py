d645baa0a984fdc0a793211a4767c25572b59e8d
Franck Michea <franck.michea@gmail.com>
2013-09-04 19:36:22 +0200

    Fix main to use generate_graph.

--- bracoujl/main.py
+++ bracoujl/main.py
@@ -7,18 +7,29 @@ import argparse
 import os
 import sys
 
-import bracoujl.graph
-import bracoujl.writers.dotwriter
-import bracoujl.writers.asciiwriter
+import bracoujl.graph as bg
+#import bracoujl.writers.dotwriter as bwd
+#import bracoujl.writers.asciiwriter
 
 def main():
     parser = argparse.ArgumentParser(description='Some debugging tool.')
     parser.add_argument('-o', '--output', action='store', required=True,
                         metavar='dir', help='output directory')
-    parser.add_argument('log', action='append', nargs='+',
+    parser.add_argument('log', action='store', nargs='+',
                         help='log file correctly formatted')
     args = parser.parse_args(sys.argv[1:])
 
+    output_dir = os.path.abspath(args.output)
+    if not os.path.exists(output_dir):
+        msg = 'I didn\'t find directory/symbolic link named `{path}` where '
+        msg += 'to generate the graphs.'
+        sys.exit(msg.format(path=output_dir))
+
+    for log in args.log:
+        functions = bg.Graph().generate_graph(log)
+        print('Found {} functions:'.format(len(functions)))
+        for function in functions:
+            print(' - {}'.format(function.name()))
 
 if __name__ == '__main__':
     main()
